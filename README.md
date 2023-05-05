17d623b48f1819ce4195cc6f28939f0b79d0d0a3
LeinadHunter <leinadhunter10@hotmail.com>
2023-05-05 09:43:39 +0200

    ft: Versión 1.0 de la app

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# flask_sql
+# flask-sql
+
 Aplicación web simple de balance de ingresos y gastos con Flask y SQL
+
+## Cómo lanzar el programa para usarlo
+
+1. Crear un entorno virtual
+
+   ```shell
+   # Windows
+   python -m venv env
+
+   # Mac / Linux
+   python3 -m venv env
+   ```
+
+2. Activar el entorno virtual
+
+   ```shell
+    # Windows
+    env\Scripts\activate
+
+    # Mac / Linux
+    source ./env/bin/activate
+
+3. Instalar las dependencias
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+4. Hacer una copia del archivo `.env_template` como `.env`
+
+   ```shell
+   # Windows
+   copy .env_template .env
+
+   # Mac / Linux
+   cp .env_template .env
+   ```
+
+5. Editar el archivo `.env` y cambiar los valores de
+   entorno necesarios. Por motivos de seguridad, dejar
+   la variable `DEBUG` con el valor `False`.
+
+6. Con el entorno virtual activo, lanzar la aplicación
+
+   ```shell
+   flask run
+   ```
+
+## Cómo lanzar el programa en desarrollo
+
+1. Crear un entorno virtual
+
+   ```shell
+   # Windows
+   python -m venv env
+
+   # Mac / Linux
+   python3 -m venv env
+   ```
+
+2. Activar el entorno virtual
+
+   ```shell
+    # Windows
+    env\Scripts\activate
+
+    # Mac / Linux
+    source ./env/bin/activate
+
+3. Instalar las dependencias
+
+   ```shell
+   pip install -r requirements.dev.txt
+   ```
+
+4. Hacer una copia del archivo `.env_template` como `.env`
+
+   ```shell
+   # Windows
+   copy .env_template .env
+
+   # Mac / Linux
+   cp .env_template .env
+   ```
+
+5. Editar el archivo `.env` y cambiar (o no) el valor de `DEBUG` (`True`/`False`)
---
+++ balance/views.py
@@ -0,0 +1,94 @@
+import os
+from flask import flash, redirect, render_template, request, url_for
+
+from . import app
+from .forms import MovimientoForm
+from .models import DBManager
+
+
+RUTA = os.path.join('data', 'balance.db')
+
+
+@app.route('/')
+def home():
+    """
+    Muestra una tabla con todos los movimientos.
+    """
+    db = DBManager(RUTA)
+    consulta = 'SELECT id, fecha, concepto, tipo, cantidad FROM movimientos'
+    movimientos = db.consultaSQL(consulta)
+    return render_template('inicio.html', movs=movimientos)
+
+
+@app.route('/nuevo', methods=['GET', 'POST'])
+def nuevo():
+    """
+    GET: muestra el formulario vacío
+    POST: recoge los datos del formulario,
+          crea el movimiento en la base de datos,
+          vuelve a la página de inicio
+    TODO: modificar el template del formulario para que envíe los datos a /modificar o /nuevo
+          (el action del formulario debe ser dinámico)
+    """
+    if request.method == 'GET':
+        formulario = MovimientoForm()
+        return render_template('form_movimiento.html', form=formulario)
+    return 'TODO: tratar POST para agregar movimiento'
+
+
+@app.route('/modificar/<int:id>', methods=['GET', 'POST'])
+def actualizar(id):
+    """
+    Actualiza los datos de un movimiento.
+    """
+    if request.method == 'GET':
+        db = DBManager(RUTA)
+        movimiento = db.obtenerMovimiento(id)
+        formulario = MovimientoForm(data=movimiento)
+        return render_template('form_movimiento.html', form=formulario, id=id)
+    elif request.method == 'POST':
+        form = MovimientoForm(data=request.form)
+        if form.validate():
+            db = DBManager(RUTA)
+            consulta = 'UPDATE movimientos SET fecha=?, concepto=?, tipo=?, cantidad=? WHERE id=?'
+            params = (
+                form.fecha.data,
+                form.concepto.data,
+                form.tipo.data,
+                form.cantidad.data,
+                form.id.data
+            )
+            resultado = db.consultaConParametros(consulta, params)
+            if resultado:
+                # genero un mensaje flash que se va a mostrar en la siguiente página
+                flash('El movimiento se ha actualizado correctamente',
+                      category='exito')
+                return redirect(url_for('home'))
+            return render_template(
+                'form_movimiento.html',
+                form=form,
+                id=id,
+                errores=['El movimiento no se ha podido guardar en la base de datos'])
+        else:
+            return render_template(
+                'form_movimiento.html',
+                form=form,
+                id=id,
+                errores=["Los datos no son válidos"])
+
+
+@app.route('/borrar/<int:id>')
+def eliminar(id):
+    """
+    Elimina un movimiento.
+    TODO: En lugar de mostrar otro template, usar un mensaje flash
+    TODO: Al pulsar el botón "papelera" en lugar de eliminar directamente,
+          mostrar una página que pida confirmación.
+          Debe tener:
+            - Mensaje pidiendo confirmación
+            - Botón "Eliminar" elimina el movimiento y vuelve a la página de inicio
+            - Botón "Cancelar" vuelve a la página de inicio sin eliminar el movimiento
+    """
+    db = DBManager(RUTA)
+    esta_borrado = db.borrar(id)
+    return render_template('borrado.html', resultado=esta_borrado)
